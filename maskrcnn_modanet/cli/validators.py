a74369a72d7f254027715626257a94b9f6044030
Pier Carlo Cadoppi <pc.cadoppi@gmail.com>
2019-05-15 16:14:19 +0200

    fixed relative path on download directory
    This closes #1, closes #2, closes #3

--- maskrcnn_modanet/cli/validators.py
+++ maskrcnn_modanet/cli/validators.py
@@ -5,6 +5,8 @@ import os
 
 def check_if_folder_exists(ctx, param, value):
 	""" check_if_folder_exists and if not, create it """
+	# making path absolute
+	value = os.path.abspath(value)
 	if not os.path.exists(value):
 		os.makedirs(value)
 	
