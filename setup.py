3cec4bec42c2c2e7c1efc9314ada1901af9b45ca
Pier Carlo Cadoppi <piercarlocadoppi@energiaemobilitasostenibile.org>
2019-07-20 23:45:55 +0200

    fixed #12, testing if it works

--- setup.py
+++ setup.py
@@ -1,44 +1,47 @@
 import setuptools
 
 with open("README.MD", "r") as fh:
-    long_description = fh.read()
+		long_description = fh.read()
 
 
 setuptools.setup(
-    name='maskrcnn-modanet',  
-    version='1.0.1.1',
-    #scripts=['maskrcnn-modanet'] ,
-    author="Pier Carlo Cadoppi",
-    author_email="piercarlo.cadoppi@studenti.unipr.it",
-    description="A MaskRCNN Keras implementation with Modanet annotations on the Paperdoll dataset",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cad0p/maskrcnn-modanet",
-    packages= ['maskrcnn_modanet'] , #setuptools.find_packages(),
-    classifiers=[
-     "Programming Language :: Python :: 3",
-     "License :: OSI Approved :: MIT License",
-     "Operating System :: OS Independent",
-    ],
-    install_requires=[
-      'keras-maskrcnn',
-      'click',
-      'progressbar',
-      'lmdb',
-      'pandas',
-      'sqlalchemy',
-      'Cython',
-      'cython',
-      'numpy',
-      'pycocotools',
-      'matplotlib',
-      'tensorflow',
-      'instaloader'
-    ],
-    entry_points = {
-        'console_scripts': [
-            'maskrcnn-modanet=maskrcnn_modanet.cli.main:main',
-        ],
-    }
+		name='maskrcnn-modanet',  
+		version='1.0.1.1',
+		# scripts=[
+		# 	'maskrcnn_modanet/cli/main.py'
+		# ],
+		author="Pier Carlo Cadoppi",
+		author_email="piercarlo.cadoppi@studenti.unipr.it",
+		description="A MaskRCNN Keras implementation with Modanet annotations on the Paperdoll dataset",
+		long_description=long_description,
+		long_description_content_type="text/markdown",
+		url="https://github.com/cad0p/maskrcnn-modanet",
+		# packages= ['maskrcnn_modanet'] , 
+		packages= setuptools.find_packages(),
+		classifiers=[
+		 "Programming Language :: Python :: 3",
+		 "License :: OSI Approved :: MIT License",
+		 "Operating System :: OS Independent",
+		],
+		install_requires=[
+			'keras-maskrcnn',
+			'click',
+			'progressbar',
+			'lmdb',
+			'pandas',
+			'sqlalchemy',
+			'Cython',
+			'cython',
+			'numpy',
+			'pycocotools',
+			'matplotlib',
+			'tensorflow',
+			'instaloader'
+		],
+		entry_points = {
+		    'console_scripts': [
+		        'maskrcnn-modanet=maskrcnn_modanet.cli.main:main',
+		    ],
+		}
 
  )
